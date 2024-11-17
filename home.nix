4bd4d189e414b8ddb30ed5debd88b150010fd40c
Liam Debell <liamdebell@Liams-MacBook-Pro.local>
2024-11-17 15:53:00 +0000

    update home-manager stateVersion

--- home.nix
+++ home.nix
@@ -61,7 +61,7 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "24.05";
+  home.stateVersion = "24.11";
 
   home.file = {
     ".zshrc".source = ./zsh/.zshrc;
